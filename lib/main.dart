73d1891f34b77df8fed03e8c046c7faa9a61ebc7
Hosain Arif <mdarifhosain214@gmail.com>
2023-12-14 11:54:41 +0600

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:whatsapp_clone/screens/home_screen.dart';
+import 'package:whatsapp_clone/view/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,13 +14,11 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Whatsapp Clone',
       theme: ThemeData(
-        primaryColor: Colors.teal,
-
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        primaryColor: const Color(0XFF075f54),
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color(0XFF128C7E)),
         useMaterial3: true,
       ),
       home: const HomeScreen(),
     );
   }
 }
-
